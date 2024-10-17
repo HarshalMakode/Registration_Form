147017b33959c756351f684774ba3a276e7b3436
HarshalMakode <harshalmakode26@gmial.com>
2024-10-18 00:20:47 +0530

    firt

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,8 @@ require('./Models/db');
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-    origin: 'https://registration-form-frontend-seven.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    origin: 'https://registration-form-frontend-seven.vercel.app'
+  }));
 
 app.get('/ping',(req,res)=>{
     res.send('PONG');
